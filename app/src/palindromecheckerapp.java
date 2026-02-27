ce79c43ced1525f8b4a95d3abb194adb55be8e31
udaybhaskar990 <languudaybhaskar@gmail.com>
2026-02-27 09:25:45 +0530

    Uday bhaskar added >> udaybhaskar990

---
+++ app/src/palindromecheckerapp.java
@@ -0,0 +1,12 @@
+public class palindromecheckerapp {
+    static final String APP_NAME = "palindrome checker app";
+    static final String VERSION = "version 1.0";
+
+ public static void main(String[] args){
+     System.out.println("=================================");
+     System.out.println("WELCOME TO PALINDROME CHECKER APP");
+     System.out.println(" " + VERSION);
+     System.out.println("=================================");
+     System.out.println("System initialized successfully.");
+ }
+}
