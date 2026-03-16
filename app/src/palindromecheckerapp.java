25188374869b97c5c3afba7456769f862afedeb7
udaybhaskar990 <languudaybhaskar@gmail.com>
2026-03-16 10:06:31 +0530

    uc12

--- app/src/palindromecheckerapp.java
+++ app/src/palindromecheckerapp.java
@@ -1,30 +1,62 @@
-class PalindromeChecker {
+import java.util.Stack;
+
+interface PalindromeStrategy {
+    boolean checkPalindrome(String word);
+}
+
+class StackStrategy implements PalindromeStrategy {
 
     public boolean checkPalindrome(String word) {
 
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+
         String reversed = "";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        while (!stack.isEmpty()) {
+            reversed = reversed + stack.pop();
         }
 
         return word.equals(reversed);
     }
 }
 
-class PalindromeCheckerApp {
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean checkPalindrome(String word) {
+
+        int start = 0;
+        int end = word.length() - 1;
+
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
+
+ class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         String word = "madam";
 
-        PalindromeChecker checker = new PalindromeChecker();
+        PalindromeStrategy strategy;
 
-        if (checker.checkPalindrome(word)) {
+        strategy = new StackStrategy();   // choose algorithm here
+
+        if (strategy.checkPalindrome(word)) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is not a Palindrome");
         }
-
     }
 }
