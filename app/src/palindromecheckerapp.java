8ccb7558c7f31c581a51806e2e489d247bd9f3f8
udaybhaskar990 <languudaybhaskar@gmail.com>
2026-03-16 09:40:31 +0530

    uc7

--- app/src/palindromecheckerapp.java
+++ app/src/palindromecheckerapp.java
@@ -1,12 +1,35 @@
-public class palindromecheckerapp {
-    static final String APP_NAME = "palindrome checker app";
-    static final String VERSION = "version 1.0";
+import java.util.Deque;
+import java.util.ArrayDeque;
 
- public static void main(String[] args){
-     System.out.println("=================================");
-     System.out.println("WELCOME TO PALINDROME CHECKER APP");
-     System.out.println(" " + VERSION);
-     System.out.println("=================================");
-     System.out.println("System initialized successfully.");
- }
+class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        while (deque.size() > 1) {
+
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
+
+    }
 }
