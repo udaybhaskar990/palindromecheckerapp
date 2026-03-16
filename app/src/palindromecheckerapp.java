cb8e67b2ae2362809ab4b482bb1a4387a72841b4
udaybhaskar990 <languudaybhaskar@gmail.com>
2026-03-16 09:46:13 +0530

    uc8

--- app/src/palindromecheckerapp.java
+++ app/src/palindromecheckerapp.java
@@ -1,28 +1,64 @@
-import java.util.Deque;
-import java.util.ArrayDeque;
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
 class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         String word = "madam";
-        Deque<Character> deque = new ArrayDeque<>();
+
+        Node head = null;
+        Node tail = null;
 
         for (int i = 0; i < word.length(); i++) {
-            deque.addLast(word.charAt(i));
+            Node newNode = new Node(word.charAt(i));
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
         }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node prev = null;
+        Node current = slow;
+
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        Node firstHalf = head;
+        Node secondHalf = prev;
 
         boolean isPalindrome = true;
 
-        while (deque.size() > 1) {
-
-            char front = deque.removeFirst();
-            char rear = deque.removeLast();
-
-            if (front != rear) {
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
                 isPalindrome = false;
                 break;
             }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
         }
 
         if (isPalindrome) {
@@ -30,6 +66,5 @@ class PalindromeCheckerApp {
         } else {
             System.out.println(word + " is not a Palindrome");
         }
-
     }
 }
